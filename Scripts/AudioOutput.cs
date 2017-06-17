0e9d2e8342de108af502a18e522a18d936cfdb1b
Daniel Rothmann <danielrothmann@me.com>
2017-06-17 18:11:53 +0200

    Moved to scripts folder

---
+++ Scripts/AudioOutput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioOutput : MonoBehaviour {
+
+    public SynthModule signalIn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnAudioFilterRead(float[] data, int channels)
+    {
+        if (signalIn != null)
+        {
+            signalIn.ProcessBuffer(data, channels);
+        }
+    }
+}
